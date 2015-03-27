d4cae2ed63924510ac8ffa1446207b0666275ff4
Victor Archila <varchila92@gmail.com>
2015-03-26 20:10:02 -0430

    Corrige la internacionalizacion de la etiqueta taxonomias por categorias.

--- app/overrides/shared_sidebar.rb
+++ app/overrides/shared_sidebar.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path => "spree/shared/_sidebar",
                  :replace => "#sidebar",
                  :text => "                     
 <aside id='categories_nav'>
-  <h3><%= Spree.t(:taxonomies) %></h3>
+  <h3><%= Spree.t(:category) %></h3>
   <%= yield :sidebar %>
 </aside>
                  ")
