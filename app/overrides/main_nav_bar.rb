579b1cf9f6c4deabf70c2434437bed639694e483
Victor Archila <varchila92@gmail.com>
2015-03-26 20:05:25 -0430

    Corrige la informacion mostrada en el banner del portal en el FrontEnd.

--- app/overrides/main_nav_bar.rb
+++ app/overrides/main_nav_bar.rb
@@ -3,14 +3,18 @@ Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                  :replace => "nav.col-md-12",
                  :text => "
 <div class='grid_3 '>
-  <div class='phone_top'>
-    <span>Call Us +777 (100) 1234</span>
+  <div class='mail_top'>
+    <span><%= Spree::Config.mails_from %></span>
   </div><!-- .phone_top -->
 </div><!-- .grid_3 -->
 
 <div class='grid_6'>
   <div class='welcome'>
-    <%= Spree.t(:welcome_visitor) %> <%= link_to Spree.t(:login), login_path %> / <%= link_to Spree.t(:create_a_new_account), signup_path  %>.
+    <% if spree_current_user %>
+      <%= Spree.t(:welcome_user, user: spree_current_user.email) %>
+    <% else %>
+      <%= Spree.t(:welcome_visitor) %> <%= link_to Spree.t(:login), login_path %> / <%= link_to Spree.t(:create_a_new_account), signup_path  %>.
+    <% end %>
   </div><!-- .welcome -->
 </div><!-- .grid_6 -->
 
