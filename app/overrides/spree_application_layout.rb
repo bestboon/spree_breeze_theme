9081a9b52417d6cdf151ae51c2694cf1a9931a9c
Victor Archila <varchila92@gmail.com>
2015-03-26 20:11:12 -0430

    Corrige la plantilla de layout de la aplicacion para que se ajuste al thema Breeze

--- app/overrides/spree_application_layout.rb
+++ app/overrides/spree_application_layout.rb
@@ -1,15 +1,23 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                  :name => "spree_application_layout",
-                 :replace => "div.container",
-                 :text => "                     
-<div class='container_12'>
-    <%= breadcrumbs(@taxon) %>
-    <div id='sidebar' class='grid_3'>
-    	<%= render partial: 'spree/shared/sidebar' if content_for? :sidebar %>
-    </div>
-      <%= flash_messages %>
-      <%= yield %>
+                 :replace => "body",
+                 :text => "
+<body id='<%= @body_id || 'default' %>' data-hook='body'>
+  <%= render partial: '/spree/shared/google_analytics' %>
+  <%= render partial: '/spree/shared/header' %>                 
+  <section id='main'>
+    <div class='container_12'>
+        <%= breadcrumbs(@taxon) %>
+        <div id='sidebar' class='grid_3'>
+          <%= render partial: 'spree/shared/sidebar' if content_for? :sidebar %>
+        </div>
+          <%= flash_messages %>
+          <%= yield %>
 
-    <%= yield :templates %>
-</div>                   
+        <%= yield :templates %>
+    </div>                   
+  </section>
+  <div class='clear'></div>
+  <%= render :partial => 'spree/shared/footer' %>
+</body>                
                  ")
